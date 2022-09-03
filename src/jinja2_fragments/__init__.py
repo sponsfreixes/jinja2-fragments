c9b10399fc40a0d22d8741c36306c465bb443e3f
Sergi Pons Freixes <sponsfreixes@users.noreply.github.com>
2022-09-03 12:12:02 -0700

    Add render_block function
    This will render a block inside a template.

---
+++ src/jinja2_fragments/__init__.py
@@ -0,0 +1,77 @@
+import typing
+
+from jinja2 import Environment
+
+
+class BlockNotFoundError(Exception):
+    pass
+
+
+async def render_block_async(
+    environment: Environment,
+    template_name: str,
+    block_name: str,
+    *args: typing.Any,
+    **kwargs: typing.Any,
+) -> str:
+    """
+    This works similar to :func:`render_block` but returns a coroutine that when
+    awaited returns the entire rendered template block string. This requires the
+    environment async feature to be enabled.
+    """
+    if not environment.is_async:
+        raise RuntimeError("The environment was not created with async mode enabled.")
+
+    template = environment.get_template(template_name)
+    block_render_func = template.blocks[block_name]
+    ctx = environment.new_context(dict(*args, **kwargs))
+    try:
+        return environment.concat(  # type: ignore
+            [n async for n in block_render_func(ctx)]  # type: ignore
+        )
+    except Exception:
+        return environment.handle_exception()
+
+
+def render_block(
+    environment: Environment,
+    template_name: str,
+    block_name: str,
+    *args: typing.Any,
+    **kwargs: typing.Any,
+) -> str:
+    """This returns the rendered template block as a string."""
+    if environment.is_async:
+        import asyncio
+
+        close = False
+
+        try:
+            loop = asyncio.get_running_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            close = True
+
+        try:
+            return loop.run_until_complete(
+                render_block_async(
+                    environment, template_name, block_name, *args, **kwargs
+                )
+            )
+        finally:
+            if close:
+                loop.close()
+
+    template = environment.get_template(template_name)
+    try:
+        block_render_func = template.blocks[block_name]
+    except KeyError:
+        raise BlockNotFoundError(
+            f"Block '{block_name}' not found on template '{template_name}'"
+        )
+
+    ctx = template.new_context(dict(*args, **kwargs))
+    try:
+        return environment.concat(block_render_func(ctx))  # type: ignore
+    except Exception:
+        environment.handle_exception()
