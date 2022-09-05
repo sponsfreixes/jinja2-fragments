6b7b22447c4f66713b2413dd9f186f53a44d7325
Sergi Pons Freixes <sponsfreixes@users.noreply.github.com>
2022-09-04 17:12:03 -0700

    Add PyPI classifiers

--- setup.py
+++ setup.py
@@ -26,4 +26,16 @@ setup(
     zip_safe=False,
     extras_require={"dev": dev_requires, "tests": tests_requires},
     install_requires=requires,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Flask",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
 )
