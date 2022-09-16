459abaece52e404bb2d1a62341674ab9e9bb025d
Mario Munoz <pythonbynight@gmail.com>
2022-09-16 00:20:18 -0700

    Create Jinja2Blocks

--- src/jinja2_fragments/fastapi.py
+++ src/jinja2_fragments/fastapi.py
@@ -1,62 +1,51 @@
 import typing
-from functools import wraps
 
 try:
-    from fastapi.templating import Jinja2Templates
+    from starlette.background import BackgroundTask
+    from starlette.templating import Jinja2Templates, _TemplateResponse
 except ModuleNotFoundError as e:
     raise ModuleNotFoundError(
-        "Install FastAPI before using jinja2_fragments.fastapi"
+        "Install Starlette to use jinja2_fragments.fastapi"
     ) from e
 
-import jinja2_fragments
+from . import render_block
 
 
 class InvalidContextError(Exception):
     pass
 
 
-def render_block(
-    jinja2templates: Jinja2Templates,
-    *,
-    block_name: typing.Optional[str] = None,
-    **context: typing.Any,
-):
-    """Renders the template's block from the template directory defined in
-    `jinja2templates` object, with the given context.
+class Jinja2Blocks(Jinja2Templates):
+    def __init__(self, directory, **env_options):
+        super().__init__(directory, **env_options)
 
-    :param jinja2templates: the FastAPI `Jinja2Templates` object that mounts the
-    app template directory
-    :param block_name: the name of the block to be rendered
-    :param context: the variables that should be available in the context of the block
-    """
+    def TemplateResponse(
+        self,
+        name: str,
+        context: dict,
+        status_code: int = 200,
+        headers: typing.Optional[typing.Mapping[str, str]] = None,
+        media_type: typing.Optional[str] = None,
+        background: typing.Optional[BackgroundTask] = None,
+        *,
+        block_name: typing.Optional[str] = None,
+    ) -> typing.Union[_TemplateResponse, str]:
+        if "request" not in context:
+            raise ValueError('context must include a "request" key')
+        template = self.get_template(name)
 
-    def inner(func):
-        env = jinja2templates.env  # Refers to Jinja's `Environment.get` object
-
-        @wraps(func)
-        async def response_method(*args, **kwargs):
-
-            # extract variables from FastAPI TemplateResponse
-            func_response = await func(*args, **kwargs)
-            func_context = func_response.context
-            template_name = func_response.template.name
-
-            # Combine context of TemplateResponse and method kwargs
-            context.update(func_context)
-
-            # Return FastAPI TemplateResponse if no block_name given
-            if not block_name:
-                return jinja2templates.TemplateResponse(template_name, context)
-
-            if isinstance(context, dict):
-                return jinja2_fragments.render_block(
-                    env, template_name, block_name, context
-                )
-            else:
-                raise InvalidContextError(
-                    f"Context of type {type(context)} is not valid. Expected dict."
-                )
-
-        return response_method
-
-    return inner
+        if block_name:
+            return render_block(
+                self.env,
+                name,
+                block_name,
+                context,
+            )
+        return _TemplateResponse(
+            template,
+            context,
+            status_code=status_code,
+            headers=headers,
+            media_type=media_type,
+            background=background,
+        )
