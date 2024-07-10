9da6cf6b2d70ad3d9d75188c00e78043e8c97682
Ronald Huggler <ronald@huggler.com.br>
2024-07-10 18:18:47 -0300

    Add render_block_list function
    This will render multiple blocks inside a template and concatenate them
    all to return in a single response. This enables easier [out-of-band
    updates](https://htmx.org/attributes/hx-swap-oob/).

--- src/jinja2_fragments/__init__.py
+++ src/jinja2_fragments/__init__.py
@@ -85,3 +85,57 @@ def render_block(
         return environment.concat(block_render_func(ctx))  # type: ignore
     except Exception:
         environment.handle_exception()
+
+
+def render_block_list(
+    environment: Environment,
+    template_name: str,
+    block_name_list: list[str],
+    *args: typing.Any,
+    **kwargs: typing.Any,
+) -> str:
+    """This returns one or more rendered template blocks as a string."""
+    if environment.is_async:
+        import asyncio
+
+        close = False
+
+        try:
+            loop = asyncio.get_running_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            close = True
+
+        try:
+            raise NotImplementedError()
+        finally:
+            if close:
+                loop.close()
+
+    return _render_template_blocks(
+        environment, template_name, block_name_list, *args, **kwargs
+    )
+
+
+def _render_template_blocks(
+    environment: Environment,
+    template_name: str,
+    block_name_list: list[str],
+    *args: typing.Any,
+    **kwargs: typing.Any,
+) -> str:
+    contents: list[str] = []
+    template = environment.get_template(template_name)
+
+    for block_name in block_name_list:
+        try:
+            block_render_func = template.blocks[block_name]
+        except KeyError:
+            raise BlockNotFoundError(block_name, template_name)
+
+        ctx = template.new_context(dict(*args, **kwargs))
+        try:
+            contents.append(environment.concat(block_render_func(ctx)))  # type: ignore
+        except Exception:
+            environment.handle_exception()
+    return "".join(contents)
